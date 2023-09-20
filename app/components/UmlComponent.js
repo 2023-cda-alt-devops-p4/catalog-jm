e98fc060f460f87964222cc334b5fc94fffe0be5
Jonathan Moreschi <j.moreschi@outlook.fr>
2023-09-20 18:06:37 +0200

    feat: Component to display UML diagrams
    Creation of the coomponent which will display the uml diagrams and the json file which will store them

---
+++ app/components/UmlComponent.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Nobile } from 'next/font/google'
+
+const nobile = Nobile({
+  subsets: ['latin'],
+  weight: '400',
+})
+
+const UmlComponent = (item) => {
+  return (
+    <section>
+      <h3>{item.title}</h3>
+      <h4 className={[nobile.className].join(' ')}>{item.description}</h4>
+      {item.elements.map((el, i) => {
+        return (
+          <div key={i}>
+            <h4 className={[nobile.className].join(' ')}>{el.name}</h4>
+            <p>{el.url}</p>
+          </div>
+        )
+      })}
+      <h4 className={[nobile.className].join(' ')}>{item.example.details}</h4>
+      <p>{item.example.url}</p>
+    </section>
+  )
+}
+
+export default UmlComponent
--- app/uml/page.js
+++ app/uml/page.js
@@ -1,36 +1,16 @@
 import React from 'react'
 import styles from '../page.module.scss'
-import { Nobile } from 'next/font/google'
-
-const nobile = Nobile({
-  subsets: ['latin'],
-  weight: '400',
-})
+import uml from '../ressources/uml.json'
+import UmlComponent from '../components/UmlComponent'
 
 const Uml = () => {
+  const umlData = uml.map((item, index) => <UmlComponent item={item} key={index} /> )
   return (
     <main className={styles.main}>
       <h2>
         UML
       </h2>
-      <section>
-        <h3>Diagramme de classes</h3>
-        <h4 className={[nobile.className].join(' ')}>Description</h4>
-        <h4 className={[nobile.className].join(' ')}>Les élements</h4>
-        <h4 className={[nobile.className].join(' ')}>Exemple</h4>
-      </section>
-      <section>
-        <h3>Diagramme de séquence</h3>
-        <h4 className={[nobile.className].join(' ')}>Description</h4>
-        <h4 className={[nobile.className].join(' ')}>Les élements</h4>
-        <h4 className={[nobile.className].join(' ')}>Exemple</h4>
-      </section>
-      <section>
-        <h3>Diagramme de cas d'utilisation</h3>
-        <h4 className={[nobile.className].join(' ')}>Description</h4>
-        <h4 className={[nobile.className].join(' ')}>Les élements</h4>
-        <h4 className={[nobile.className].join(' ')}>Exemple</h4>
-      </section>
+      {umlData}
     </main>
   )
 }
