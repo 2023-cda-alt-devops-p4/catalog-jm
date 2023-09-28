89f296634d3546ba76d7ca706ee228298d09cc04
Jonathan Moreschi <j.moreschi@outlook.fr>
2023-09-28 17:45:55 +0200

    Feat : Style section
    Begin to apply style to individual section

--- app/components/UmlComponent.js
+++ app/components/UmlComponent.js
@@ -3,6 +3,7 @@ import React, { useContext } from 'react'
 import Image from 'next/image'
 import { elementContext } from '@/context/elementCtx'
 import { Nobile } from 'next/font/google'
+import styles from '../component.module.scss'
 
 const nobile = Nobile({
   subsets: ['latin'],
@@ -12,29 +13,29 @@ const nobile = Nobile({
 const UmlComponent = ({ item }) => {
   const ratio = 16 / 9
   // import context to change the content of the element div
-  const {state, dispatch} = useContext(elementContext)
+  const { state, dispatch } = useContext(elementContext)
 
   return (
-    <section id={item.id}>
+    <section id={item.id} className={styles.individualComponent}>
       <div>
         <h3>{item.title}</h3>
         <h4 className={[nobile.className].join(' ')}>{item.description}</h4>
       </div>
       <div>
-        <div onClick={() => dispatch({type:'hydrate', payload: item.elements})}>Montrer la légende</div>
-
+        <div onClick={() => dispatch({ type: 'hydrate', payload: item.elements })}>Montrer la légende</div>
       </div>
       {
         item.example.url !== '' ?
-          <div>
-            <Image
-              src={item.example.url}
-              width={400}
-              height={400 / ratio}
-              alt={item.title}
-            />
+          <>
+            <div className={styles.imgExample}>
+              <Image
+                src={item.example.url}
+                fill
+                alt={item.title}
+              />
+            </div>
             <p className={[nobile.className].join(' ')}>{item.example.details}</p>
-          </div>
+          </>
           : null
       }
     </section>
--- app/components/UmlDisplay.js
+++ app/components/UmlDisplay.js
@@ -1,17 +1,18 @@
 import React from 'react'
 import UmlComponent from './UmlComponent'
+import styles from '../component.module.scss'
+
 
 const UmlDisplay = ({item}) => {
   const umlData = item.display.map((item, index) => {
     return (
-
       <UmlComponent item={item} key={index} />
     )
 })
 
   return (
-    <div>
-      <h3>
+    <div className={styles.componentDisplay}>
+      <h3 className={styles.componentTitle}>
         {item.type}
       </h3>
       {umlData}
