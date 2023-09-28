355fd5d2f38c9a669406737dc323fe1a9f35f601
Jonathan Moreschi <j.moreschi@outlook.fr>
2023-09-28 09:52:52 +0200

    Feat : Creating context
    Creation of a context to avoid props drillind to show the elements by diagrams/models

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,7 @@ import Footer from './components/Footer'
 import Navbar from './components/Navbar'
 import './globals.scss'
 import { Metrophobic } from 'next/font/google'
-
+import { ElementContextProvider } from '@/context/elementCtx'
 const metrophobic = Metrophobic({
   subsets: ['latin'],
   weight: '400',
@@ -13,12 +13,15 @@ const metrophobic = Metrophobic({
 export default function RootLayout({ children }) {
 
   const [darkTheme, setDarkTheme] = useState('dark')
+
   return (
     <html lang="fr">
       <body className={metrophobic.className} data-theme-ui={darkTheme}>
-        <Navbar darkTheme={darkTheme} setDarkTheme={()=> setDarkTheme(prev => prev === 'light' ? 'dark': 'light')} />
-        {children}
-        <Footer />
+        <ElementContextProvider>
+          <Navbar darkTheme={darkTheme} setDarkTheme={() => setDarkTheme(prev => prev === 'light' ? 'dark' : 'light')} />
+          {children}
+          <Footer />
+        </ElementContextProvider>
       </body>
     </html>
   )
---
+++ context/elementCtx.js
@@ -0,0 +1,30 @@
+import { createContext, useMemo, useReducer } from "react"
+
+const initialState = [{
+}]
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'hydrate':
+      state = action.payload
+      return state
+    case 'init':
+      return initialState
+  }
+}
+
+export const elementContext = createContext(initialState)
+
+export const ElementContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  const contextValue = useMemo(() => {
+    return { state, dispatch }
+  }, [state, dispatch])
+
+  return (
+    <elementContext.Provider value={contextValue}>
+      {children}
+    </elementContext.Provider>
+  )
+}
