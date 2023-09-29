f517f500ad8cff8618272ed033c07e4b453c7b08
Jonathan Moreschi <j.moreschi@outlook.fr>
2023-09-29 16:13:14 +0200

    Feat: Styling link in footer

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -8,9 +8,9 @@ const Footer = () => {
       <p>&copy;Jonathan Moreschi</p>
       <div className={styles.sources}>
         <h3>Sources : </h3>
-        <Link href={'https://www.uml-diagrams.org/'} target='_blank'>Documentation UML</Link>
-        <Link href={'https://www.youtube.com/playlist?list=PL6JepipuvDNKiH_epQq7ZcfWERsyxoGDJ'} target='_blank'>Playlist youtube F.BAURAND</Link>
-        <Link href={'https://cours-ofppt.weebly.com/uploads/1/3/0/8/13086652/merise.pdf'} target='_blank'>Cours MERISE S.ELACHOURI</Link>
+        <Link href={'https://www.uml-diagrams.org/'} className={styles.footerLink} target='_blank'>Documentation UML</Link>
+        <Link href={'https://www.youtube.com/playlist?list=PL6JepipuvDNKiH_epQq7ZcfWERsyxoGDJ'} className={styles.footerLink} target='_blank'>Playlist youtube F.BAURAND</Link>
+        <Link href={'https://cours-ofppt.weebly.com/uploads/1/3/0/8/13086652/merise.pdf'} className={styles.footerLink} target='_blank'>Cours MERISE S.ELACHOURI</Link>
       </div>
     </footer>
   )
